63e215f13da711b0719d154f0eb7f7c7a6572aaa
Pito Salas <pitosalas@gmail.com>
2012-07-26 20:59:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repeatsurvey::Application.config.secret_token = '61a27f9275d5d361eb3e9f53a2d2817b7499246c95e1e9a61a55a7f6c373a9d6c99d886020f16edd9c7bd4186f32d14a86627c97b01673dac3895588ad81d1cf'
